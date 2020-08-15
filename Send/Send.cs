5de7b27f97531f1d91239f7a5c1cac3624ba2804
lhcardoso <c.luizhenrique@bmail.com>
2020-08-15 17:51:32 -0300

    done tutorial

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace Send
 {
-    class Program
+    class Send
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable:false,
+                    exclusive: false,
+                    autoDelete:false,
+                    arguments: null
+                    );
+
+                var message = "Hello World";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange:"",
+                    routingKey:"hello",
+                    basicProperties:null,
+                    body:body
+                    );
+
+                Console.WriteLine($" [x] Sent {message}");
+            }
+            Console.WriteLine(" Press [enter] to exit");
         }
     }
 }
